7c03dd5098d6b0199ab1a206828dae1794865997
Jefry Navas <jnavass@est.ups.edu.ec>
2020-06-24 08:17:45 -0500

    Agregado otro archivo en Git

--- script.py
+++ script.py
@@ -5,7 +5,8 @@ Fecha:24/06/2020
 """
 
 def main():
-    print("Hola Mundo")
+    nombre= input("Ingrese tu nombre: ")
+    print("Hola ",nombre," Bienvenido a Git")
 
 if __name__ == "__main__":
     main()
